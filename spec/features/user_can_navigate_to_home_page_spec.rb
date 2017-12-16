a376770f28d156500d41a6d2288d8b068914c0c0
katyjane8 <katy.jane8@gmail.com>
2017-12-16 13:00:13 -0700

    starting feature tests to test heroku render

---
+++ spec/features/user_can_navigate_to_home_page_spec.rb
@@ -0,0 +1,21 @@
+require './test/test_helper'
+
+class LinkTest < CapybaraTestCase
+  def test_user_can_see_the_homepage
+    visit '/'
+    click_on "About"
+
+    assert_equal 200, page.status_code
+    assert_equal '/index', current_path
+    assert page.has_content?("About Me!")
+  end
+
+  def test_user_can_see_other_pages
+    visit '/'
+    click_on "Contact"
+
+    assert_equal 200, page.status_code
+    assert_equal '/contact', current_path
+    assert page.has_content?("Phone Number")
+  end
+end
