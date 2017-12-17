b763fdb56cffecfbecb343be8794365ef7cf1aaf
katyjane8 <katy.jane8@gmail.com>
2017-12-16 21:49:44 -0700

    new tables added for user/idea/categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171216201718) do
+ActiveRecord::Schema.define(version: 20171217044359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20171216201718) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_ideas_on_category_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+  end
+
+  add_foreign_key "ideas", "categories"
 end
