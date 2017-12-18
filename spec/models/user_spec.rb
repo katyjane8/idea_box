7509c0f377456009c772c86997b4feded0f16480
katyjane8 <katy.jane8@gmail.com>
2017-12-18 14:33:50 -0700

    user spec tests passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  # describe "validations" do
-  #   it { should validate_presence_of(:username) }
-  # end
+describe User do
+  it "can be created as an admin" do
+    user = User.create(username: "penelope",
+                       password: "boom",
+                       role: 1)
 
+    expect(user.role).to eq("admin")
+    expect(user.admin?).to be_truthy
+  end
+
+  it "can be created as a default user" do
+    user = User.create(username: "sammy",
+                       password: "pass",
+                       role: 0)
+
+    expect(user.role).to eq("default")
+    expect(user.default?).to be_truthy
+  end
 end
