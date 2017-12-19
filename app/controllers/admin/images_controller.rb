e02032240723603ea1ab8ec6904c04949608573e
katyjane8 <katy.jane8@gmail.com>
2017-12-19 14:44:04 -0700

    create image method

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -1,8 +1,24 @@
 class Admin::ImagesController < Admin::BaseController
   def index
+    @images = Image.all
   end
 
   def new
     @image = Image.new
   end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.save!
+      redirect_to admin_images_path
+    else
+      render :new
+    end
+  end
+
+    private
+
+    def image_params
+      params.require(:image).permit(:name, :url)
+    end
 end
