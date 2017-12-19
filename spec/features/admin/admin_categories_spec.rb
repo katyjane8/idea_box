aafb2900eaf903be5c56acdb8774be912b67983e
katyjane8 <katy.jane8@gmail.com>
2017-12-19 11:51:17 -0700

    factorybot added

--- spec/features/admin/admin_categories_spec.rb
+++ spec/features/admin/admin_categories_spec.rb
@@ -14,12 +14,10 @@ describe "User visits categories index page" do
 
   context "as default user" do
     it 'does not allow default user to see admin categories index' do
-      user = User.create(username: "fern@gully.com",
-                         password: "password",
-                         role: 0)
+      user = create(:user)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      
+
       visit admin_categories_path
       expect(page).to_not have_content("Admin Categories")
       expect(page).to have_content("The page you were looking for doesn't exist.")
