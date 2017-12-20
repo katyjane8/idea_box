ca3a630cbccf0d000c01933b45c9e32c57090e9d
katyjane8 <katy.jane8@gmail.com>
2017-12-19 19:21:01 -0700

    configure Paperclip

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,6 @@ class Image < ApplicationRecord
   validates :name, :url, presence: true
   has_many :ideas
 
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
