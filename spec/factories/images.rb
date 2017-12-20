c7c8a92002992fb5b40c71207aaed483be32ef2a
katyjane8 <katy.jane8@gmail.com>
2017-12-20 16:07:57 -0700

    admin can add image through paperclip

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :image do
-    name "knitting"
-    url "https://pixabay.com/en/art-flowers-papers-table-wood-1867218/"
+    image_file_name "1.jpg"
+    image_content_type "image/jpeg"
+    image_file_size 2000
   end
 end
