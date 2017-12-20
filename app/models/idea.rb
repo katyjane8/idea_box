d236ce92a399c2d19ddec0551c5d79327346149c
katyjane8 <katy.jane8@gmail.com>
2017-12-20 16:09:56 -0700

    ideas show and form

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,6 +2,7 @@ class Idea < ApplicationRecord
   validates :title, :body, presence: true
   belongs_to :category
   belongs_to :user
+  has_many :idea_images
   has_many :images, through: :idea_images
 
 end
