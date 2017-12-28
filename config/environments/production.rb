11e22374067b7447594392f6e32dfda2edbf2fd7
katyjane8 <katy.jane8@gmail.com>
2017-12-28 09:09:43 -0700

    remove AWS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,16 +77,16 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Amazon S3 settings for Paperclip uploads
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_protocol: 'http',
-    s3_credentials: {
-      bucket: ENV.fetch("AWS_BUCKET"),
-      access_key_id: ENV.fetch("AWS_ACCESS_KEY_ID"),
-      secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY"),
-      s3_region: ENV.fetch("AWS_REGION"),
-    }
-  }
+  # config.paperclip_defaults = {
+  #   storage: :s3,
+  #   s3_protocol: 'http',
+  #   s3_credentials: {
+  #     bucket: ENV.fetch("AWS_BUCKET"),
+  #     access_key_id: ENV.fetch("AWS_ACCESS_KEY_ID"),
+  #     secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY"),
+  #     s3_region: ENV.fetch("AWS_REGION"),
+  #   }
+  # }
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
