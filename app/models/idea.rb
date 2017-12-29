ece00ac2e8f3ff22e68bdc583988047177754eae
katyjane8 <katy.jane8@gmail.com>
2017-12-29 10:52:44 -0700

    validate content type

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,9 +1,9 @@
 class Idea < ApplicationRecord
+  has_attached_file :image
   validates :title, :body, presence: true
   belongs_to :category, optional: true
   belongs_to :user
   has_many :idea_images
   has_many :images, through: :idea_images
-  has_attached_file :image
-  # validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
