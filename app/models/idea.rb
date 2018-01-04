db927d273b6870a978bbcb5f78e2434f2b9b5ffd
katyjane8 <katy.jane8@gmail.com>
2018-01-03 20:13:25 -0700

    final with tests passing

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,4 @@
 class Idea < ApplicationRecord
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "150x150#" }
-  validates_attachment_content_type :image, content_type: { content_type: /\Aimage\/.*\Z/ }
   validates :title, :body, presence: true
   belongs_to :category, optional: true
   belongs_to :user
